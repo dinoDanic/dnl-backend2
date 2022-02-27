26d2da719cec1b61fed8475a4f5d7b549d6d4887
Dino Danic <dino.danic@gmail.com>
2022-02-27 11:18:42 +0100

    rails tables

--- db/migrate/20220227101603_create_products.rb
+++ db/migrate/20220227101603_create_products.rb
@@ -2,8 +2,13 @@ class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.string :string
       t.string :code
+      t.string :ean
+      t.integer :weight
+      t.integer :dimension_a
+      t.integer :dimension_b
+      t.integer :dimension_c
+      t.string :description
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
